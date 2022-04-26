f2496ccbac599d099da721f0907a81b413cbd18a
Miguel Fernandes <mix_421@hotmail.com>
2022-04-26 21:33:56 +0100

    Final Commit

--- program.py
+++ program.py
@@ -4,6 +4,7 @@ sys.stdout.reconfigure(encoding='UTF-8')
 
 #Listas Ligadas
 #Models and Node
+#Modelos com itens e apontadores com referencia, diferentes tipos de operações utilizando referencias. 
 
 if __name__ == "__main__":
     v.main() 
